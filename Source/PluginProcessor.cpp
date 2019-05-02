693f59a2a39820cd8de5abd065f1c0f088b340bf
Eric George <eegee.dev@gmail.com>
2019-05-02 14:44:26 -0700

    fixed leak

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -212,18 +212,14 @@ void KadenzeAudioPluginAudioProcessor::getStateInformation (MemoryBlock& destDat
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
-//    std::unique_ptr<XmlElement> xml (new XmlElement ("ParamTutorial"));
-//    xml->setAttribute ("gain", (double) *gain);
-//    copyXmlToBinary (*xml, destData);
     
     
-//    XmlElement preset("KAP_StateInfo");
-    std::unique_ptr<XmlElement> preset(new XmlElement ("KAP_StateInfo"));
-    std::unique_ptr<XmlElement> presetBody(new XmlElement("KAP_Preset"));
+    XmlElement preset("KAP_StateInfo");
+    XmlElement* presetBody = new XmlElement("KAP_Preset");
     
-    mPresetManager->getXmlForPreset(presetBody.get());
-    preset->addChildElement(presetBody.get());
-    copyXmlToBinary(*preset, destData);
+    mPresetManager->getXmlForPreset(presetBody);
+    preset.addChildElement(presetBody);
+    copyXmlToBinary(preset, destData);
 }
 
 void KadenzeAudioPluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
